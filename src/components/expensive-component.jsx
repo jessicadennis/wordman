27ccef89336343f612ae3b8b69204d0f6de74b51
Jessica Dennis <jessica.dennis@pnnl.gov>
2024-02-14 16:33:54 -0500

    Memoize expensivecomponent for funsies

--- src/components/expensive-component.jsx
+++ src/components/expensive-component.jsx
@@ -1,12 +1,13 @@
 import { block } from '../lib/sleep';
+import { memo } from 'react';
 
 const ExpensiveComponent = () => {
   block(200);
   return (
-    <div className="px-4 py-2 font-bold text-center bg-orange-400 border border-orange-700 animate-pulse">
+    <div className="animate-pulse border border-orange-700 bg-orange-400 px-4 py-2 text-center font-bold">
       🗑️🔥 I am expensive! 🔥🗑️
     </div>
   );
 };
 
-export default ExpensiveComponent;
+export default memo(ExpensiveComponent);
