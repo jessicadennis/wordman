bc8a94ff16a88761989f6b72d3abd1af7e42d72b
Jessica Dennis <jessicadennis@BlackBookAir.fios-router.home>
2024-02-14 00:05:35 -0500

    Solve slowdown
    * Move input handling into the child component to reduce re-rendering
    * This matters less, but cache the guess checking function with useCallback
    * Better styling for take a guess button

--- src/components/game-input.jsx
+++ src/components/game-input.jsx
@@ -1,25 +1,34 @@
+import { useState } from 'react';
+
 const GameInput = ({ onSubmit, disabled, ...props }) => {
+  const [colorGuess, setColorGuess] = useState('');
   return (
     <form
       className="flex items-end"
       onSubmit={(e) => {
         e.preventDefault();
-        if (onSubmit) onSubmit(e);
+        if (onSubmit) onSubmit();
       }}
     >
       <label htmlFor="game-input">
         Enter some letters
         <input
+          value={colorGuess}
           id="game-input"
           type="text"
           maxLength={6}
-          pattern="[a-f|A-F|0-9]{6}"
+          pattern="[a-fA-F0-9]{6}"
           placeholder="C0FF33"
           disabled={disabled}
+          onChange={(e) => setColorGuess(e.target.value)}
           {...props}
         />
       </label>
-      <button className="whitespace-nowrap" type="submit" disabled={disabled}>
+      <button
+        className="whitespace-nowrap disabled:pointer-events-none disabled:cursor-not-allowed"
+        type="submit"
+        disabled={disabled}
+      >
         Take a Guess
       </button>
     </form>
